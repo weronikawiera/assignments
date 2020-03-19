b956e68614ddacfe870cf6cbaea7680cd6ce7f54
Veronika Vackova <62252018+weronikawiera@users.noreply.github.com>
2020-03-19 12:34:42 +0100

    Update hw1.py

--- hw1.py
+++ hw1.py
@@ -1,5 +1,5 @@
 from typing import List
-
+import datetime
 import pandas as pd
 
 CONFIRMED_CASES_URL = f"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data" \
@@ -13,43 +13,44 @@ confirmed_cases = pd.read_csv(CONFIRMED_CASES_URL, error_bad_lines=False)
 
 
 def poland_cases_by_date(day: int, month: int, year: int = 2020) -> int:
+
     """
     Returns confirmed infection cases for country 'Poland' given a date.
-
     Ex.
     >>> poland_cases_by_date(7, 3, 2020)
     5
     >>> poland_cases_by_date(11, 3)
     31
-
     :param year: 4 digit integer representation of the year to get the cases for, defaults to 2020
     :param day: Day of month to get the cases for as an integer indexed from 1
     :param month: Month to get the cases for as an integer indexed from 1
     :return: Number of cases on a given date as an integer
     """
     
-    # Your code goes here (remove pass)
-    pass
+    result = confirmed_cases.loc[confirmed_cases["Country/Region"]=="Poland"][f"{month}/{day}/20"].values[0]
+    return result
 
 
 def top5_countries_by_date(day: int, month: int, year: int = 2020) -> List[str]:
+
     """
     Returns the top 5 infected countries given a date (confirmed cases).
-
     Ex.
     >>> top5_countries_by_date(27, 2, 2020)
     ['China', 'Korea, South', 'Cruise Ship', 'Italy', 'Iran']
     >>> top5_countries_by_date(12, 3)
     ['China', 'Italy', 'Iran', 'Korea, South', 'France']
-
     :param day: 4 digit integer representation of the year to get the countries for, defaults to 2020
     :param month: Day of month to get the countries for as an integer indexed from 1
     :param year: Month to get the countries for as an integer indexed from 1
     :return: A list of strings with the names of the coutires
     """
 
-    # Your code goes here (remove pass)
-    pass
+    pogrupowane = confirmed_cases.groupby("Country/Region").sum()
+    result = pogrupowane.sort_values(by=f"{month}/{day}/20").tail(5)
+    lista = result.sort_values(by=f"{month}/{day}/20", ascending=False)
+    return list(lista.index)
+  
 
 # Function name is wrong, read the pydoc
 def no_new_cases_count(day: int, month: int, year: int = 2020) -> int:
